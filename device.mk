92fb1660bc62d519177c7986b40acc34ba6d1d73
Jake Weinstein <xboxlover360@gmail.com>
2014-05-07 17:20:33 -0500

    m8: Re-enable PnPMgr, thermal-engine
    PS3: Fix whitespace
    PS4: Remove pnp.xml; better performance and benchmarks
    PS5: Remove qosmgr, thermal-engine-8974.conf,
         update proprietary-files
    PS7: Add pnp.xml
    
    Change-Id: Id83ab9a540c5b3519be5d3912b31aef76bdda69a

--- device.mk
+++ device.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libdashplayer
 
+# Pnpmgr
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pnp.xml:system/etc/pnp.xml
+
 # Power
 PRODUCT_PACKAGES += \
     power.msm8974
@@ -185,6 +189,10 @@ PRODUCT_PACKAGES += \
 # QCOM rngd
 PRODUCT_PACKAGES += \
     qrngd
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
 # Torch
 PRODUCT_PACKAGES += \
